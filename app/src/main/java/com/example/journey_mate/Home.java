5a32edff0d7e6c85f39f222016585b496169aa3b
kushal shrestha <rockushal3@gmail.com>
2019-12-02 01:23:46 +0545

    profile design and home page fragment design and function of navigation drawer

--- app/src/main/java/com/example/journey_mate/Home.java
+++ app/src/main/java/com/example/journey_mate/Home.java
@@ -3,17 +3,25 @@ package com.example.journey_mate;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
 //note this import
+import com.example.journey_mate.fragment.Home_Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 
 public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
@@ -21,6 +29,9 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
     Button menu;
     ActionBarDrawerToggle drawerToggle ;
     BottomNavigationView home_navigation;
+    Home_Fragment home_fragment;
+    Fragment fragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,20 +40,45 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         //change id to Your id
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         //Drawer Menu
         drawerLayout = findViewById(R.id.drawerllayout);
         NavigationView navigationView = findViewById(R.id.navView);
         menu = findViewById(R.id.btn_menu);
         navigationView.setNavigationItemSelectedListener(this);
         drawerLayout.setDrawerListener(drawerToggle);
-
         // Top Navigation View
         home_navigation = findViewById(R.id.homeNavigation);
-
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openDrawer();
+            }
+        });
+
+        home_fragment = new Home_Fragment();
+        setFragment(home_fragment);
+        home_navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        setFragment(home_fragment);
+                        Toast.makeText(Home.this, "home", Toast.LENGTH_SHORT).show();
+                    break;
+
+                    case R.id.nav_friend:
+                        Toast.makeText(Home.this, "friend", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_addfriend:
+                        Toast.makeText(Home.this, "addfriend", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_notification:
+                        Toast.makeText(Home.this, "notification", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+                return true;
             }
         });
     }
@@ -50,7 +86,17 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         String itemName = (String)item.getTitle();
-
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.profile_nav:
+                intent = new Intent(Home.this,profile.class);
+                startActivity(intent);
+                break;
+            case R.id.home_nav:
+                intent = new Intent(Home.this,Home.class);
+                startActivity(intent);
+                break;
+        }
         CloseDrawer();
         return true;
     }
@@ -61,6 +107,12 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
 
     private void openDrawer() {
         drawerLayout.openDrawer(GravityCompat.END);
+    }
+
+    public void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment, fragment);
+        fragmentTransaction.commit();
     }
 
 
