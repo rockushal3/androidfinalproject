60de740e883dca016cb8dd9ab5fd9a80fefce3d4
kushal shrestha <rockushal3@gmail.com>
2019-12-09 09:53:27 +0545

    Structure of MVC and design review

---
+++ app/src/main/java/com/example/journey_mate/controller/Home.java
@@ -0,0 +1,150 @@
+package com.example.journey_mate.controller;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+//note this import
+import com.example.journey_mate.R;
+import com.example.journey_mate.controller.fragment.Friend_Fragment;
+import com.example.journey_mate.controller.fragment.Friend_Request_Fragment;
+import com.example.journey_mate.controller.fragment.Home_Fragment;
+import com.example.journey_mate.controller.fragment.NotificationFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    DrawerLayout drawerLayout;
+    Button menu;
+    ActionBarDrawerToggle drawerToggle ;
+
+    BottomNavigationView home_navigation;
+    Home_Fragment home_fragment;
+    Friend_Fragment friend_fragment;
+    Friend_Request_Fragment friend_request_fragment;
+    NotificationFragment notificationFragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_home);
+
+        //Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Drawer Menu
+        drawerLayout = findViewById(R.id.drawerllayout);
+        NavigationView navigationView = findViewById(R.id.navView);
+        menu = findViewById(R.id.btn_menu);
+        navigationView.setNavigationItemSelectedListener(this);
+        drawerLayout.setDrawerListener(drawerToggle);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDrawer();
+            }
+        });
+
+        //hover item selected in navigation drawer
+        MenuItem item = navigationView.getMenu().findItem(R.id.home_nav);
+        item.setCheckable(true);
+        item.setChecked(true);
+
+
+        // Top Navigation View
+        home_navigation = findViewById(R.id.homeNavigation);
+        home_fragment = new Home_Fragment();
+        setFragment(home_fragment);
+
+
+        home_navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+
+                        setFragment(home_fragment);
+                        Toast.makeText(Home.this, "home", Toast.LENGTH_SHORT).show();
+                    break;
+
+                    case R.id.nav_friend:
+                        friend_fragment = new Friend_Fragment();
+                        setFragment(friend_fragment);
+                        Toast.makeText(Home.this, "friend", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_addfriend:
+                        friend_request_fragment = new Friend_Request_Fragment();
+                        setFragment(friend_request_fragment);
+                        Toast.makeText(Home.this, "Friend Request", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_notification:
+                        notificationFragment = new NotificationFragment();
+                        setFragment(notificationFragment);
+                        Toast.makeText(Home.this, "notification", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+                return true;
+            }
+        });
+    }
+
+
+
+
+
+
+    // drawer Navigation
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.profile_nav:
+                intent = new Intent(this,profile.class);
+                startActivity(intent);
+                break;
+            case R.id.home_nav:
+                intent = new Intent(this,Home.class);
+                startActivity(intent);
+                break;
+            case R.id.logout_nav:
+                intent = new Intent(this,Login.class);
+                startActivity(intent);
+                break;
+        }
+        CloseDrawer();
+        return true;
+    }
+
+    private void CloseDrawer() {
+        drawerLayout.closeDrawer(GravityCompat.END);
+    }
+
+    private void openDrawer() {
+        drawerLayout.openDrawer(GravityCompat.END);
+    }
+
+    public void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment, fragment);
+        fragmentTransaction.commit();
+    }
+
+
+}
