7689511e69541864e00181ad88446d46195acfba
kushal shrestha <rockushal3@gmail.com>
2020-01-27 10:38:13 +0545

    Post Route Created for add post

---
+++ app/src/main/java/com/example/journey_mate/api/PostApi.java
@@ -0,0 +1,4 @@
+package com.example.journey_mate.api;
+
+public class PostApi {
+}
---
+++ app/src/main/java/com/example/journey_mate/router/PostRoute.java
@@ -0,0 +1,16 @@
+package com.example.journey_mate.router;
+
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface PostRoute {
+    @Multipart //for image
+    @POST("createpost")
+    Call<Void> createPost(@Part MultipartBody.Part img, @Part("caption") RequestBody caption, @Part("user_id") RequestBody user_id); //image file data type MultipartBody
+
+}
